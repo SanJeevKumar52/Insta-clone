371492d8cd7fc2aaf2b2940961b3dc68b90aca8e
Sanjeev <sk3616593@gmail.com>
2024-01-06 00:28:08 +0530

    ProfilePage design added succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import Pagelayout from "./Layouts/PageLayouts/Pagelayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 function App() {
   return (
     <Pagelayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path='/:username' element={<ProfilePage />} />
       </Routes>
     </Pagelayout>
   );
