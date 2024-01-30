0f522a00677afb66b6b7a918a9c0495825f9b737
Sanjeev <sk3616593@gmail.com>
2024-01-31 00:26:26 +0530

    Login using email and password done

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,10 +1,18 @@
-import { Input,Button } from "@chakra-ui/react";
+import { Input, Button, AlertIcon, Alert } from "@chakra-ui/react";
 import { useState } from "react";
+import useLogin from "../../hooks/useLogin";
+
 const Login = () => {
   const [inputs, setInputs] = useState({
     email: "",
     password: "",
   });
+  const { loading, error, login } = useLogin();
+
+  const handleLogin = () => {
+    login(inputs);
+  };
+
   return (
     <>
       <Input
@@ -23,7 +31,19 @@ const Login = () => {
         value={inputs.password}
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
-      <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+      <Button
+        w={"full"}
+        colorScheme="blue"
+        size={"sm"}
+        fontSize={14}
+        onClick={handleLogin}
+      >
         Login
       </Button>
     </>
