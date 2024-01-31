af5d65235b291446afa5f630664f4362739e2bcb
Sanjeev <sk3616593@gmail.com>
2024-02-01 01:07:14 +0530

    follow and unfollow done

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -2,10 +2,12 @@ import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from "
 import useUserProfileStore from "../../store/userProfileStore";
 import useAuthStore from '../../store/authStore'
 import EditProfile from "./EditProfile";
+import useFollowUser from "../../hooks/useFollowUser";
 const Profileheader = () => {
 	const { userProfile } = useUserProfileStore();
 	const authUser = useAuthStore(state => state.user);
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { isFollowing, isUpdating, handleFollowUser } = useFollowUser(userProfile?.uid);
 	const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
 	const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
     return (
@@ -44,8 +46,10 @@ const Profileheader = () => {
 								color={"white"}
 								_hover={{ bg: "blue.600" }}
 								size={{ base: "xs", md: "sm" }}
+								onClick={handleFollowUser}
+								isLoading={isUpdating}
 							>
-								Follow
+								{isFollowing ? "Unfollow" : "Follow"}
 							</Button>
 						</Flex>
 					)}
